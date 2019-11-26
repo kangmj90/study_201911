b692882b11cebb6d9d18f96fd8815e471bab062a
kangminjeong <rkdalseod@gmail.com>
2019-11-26 23:08:47 +0900

    Web Application 작업 추가

---
+++ board/src/main/java/com/example/WebApplication.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author kangminjeong
+ * @since 2019. 11. 26.
+ */
+@SpringBootApplication
+public class WebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+}
---
+++ board/src/main/resources/db/migration/V1.1__create_table.sql
@@ -0,0 +1,5 @@
+create table board (
+    id bigint not null,
+    name varchar(100),
+    primary key (id)
+);
